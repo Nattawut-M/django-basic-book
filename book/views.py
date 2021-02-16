65ff04ce104949355795664a7a2c57733957db7c
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-17 05:21:51 +0700

    [app/views.py] import 'Models' ,query then sent to template/ [app/template/] import 'bootstarp cdn' and show data//

--- book/views.py
+++ book/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+# import Models
+from .models import *
 
 # Create your views here.
 def index(request):
-
-    return render(request, 'book/index.html')
+    book = Book.objects.all()
+    return render(request, 'book/index.html', {'book':book})
