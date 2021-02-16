981b445148c90cf280c44e34d0cd526eea3295b2
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-16 18:04:36 +0700

    [admin.py] config admin site

--- book/admin.py
+++ book/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from book.models import *
 
+# Admin Conf
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'price', 'published']
+    list_filter = ['published', 'category', 'author', 'languages']
+    search_fields = ['name']
+    date_hierarchy = 'created'
+    prepopulated_fields = {'slug': ['name']} # auto add 'slug' from 'name
+    
+
 # Register your models here.
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Category)
 admin.site.register(Author)
 admin.site.register(LanguagesCoding)
