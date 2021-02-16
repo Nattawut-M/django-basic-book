0c3026aa8ebca7a8a99f20158dc10934c28cad2d
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-16 17:51:52 +0700

    [models.py] create new 'Model' 1.Category 2.Author 3.LanguageCoding, then set 'Relationship' of database/ [admin.py] register model to AdminStie/ [migrate]

--- book/admin.py
+++ book/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from book.models import *
+
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(LanguagesCoding)
--- book/models.py
+++ book/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class LanguagesCoding(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    
 class Book(models.Model):
     code = models.IntegerField(blank=False, null=False)
     name = models.CharField(max_length=255, blank=False, null=False)
     slug = models.SlugField(max_length=255, blank=False, null=False)
-    description = models.TextField(null=True, blank=True)
     price = models.FloatField(default=0, null=False, blank=False)
+    # relationship
+    languages = models.ForeignKey(LanguagesCoding, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ManyToManyField(Category, blank=True)
+    author = models.ManyToManyField(Author, blank=True)
+    
     published = models.BooleanField(default=True)
+    description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
