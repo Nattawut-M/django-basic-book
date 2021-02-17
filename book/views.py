bd4334ac7186f9a396cdf6dc5aa680dbd4526913
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-17 11:48:16 +0700

    [book/views.py] query data category by id of category from <query string> GET Method// [templates/book/index.html] show data by category id result// [media/upload] upload img for cover book

--- book/views.py
+++ book/views.py
@@ -6,4 +6,8 @@ from .models import *
 def index(request):
     book = Book.objects.all()
     categories = Category.objects.all()
-    return render(request, 'book/index.html', {'book':book, 'categories': categories})
+
+    category_id = request.GET.get('category')
+    if  category_id:
+        book = Book.objects.filter(category=category_id)
+    return render(request, 'book/index.html', {'book':book, 'categories': categories, 'category_id':category_id})
