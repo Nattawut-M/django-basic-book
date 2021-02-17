3d11ff79259076180bbda18cb67b873612f957ab
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-17 13:38:21 +0700

    [app/views.py] using 'Pagination' Class for pagination book page/ [template/book/index.html] show pagination

--- book/views.py
+++ book/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, InvalidPage, PageNotAnInteger, EmptyPage
 # import Models
 from .models import *
 
@@ -10,4 +11,17 @@ def index(request):
     category_id = request.GET.get('category')
     if  category_id:
         book = Book.objects.filter(category=category_id)
+
+    book_paginator = Paginator(book, 3)
+    book_page = request.GET.get('page')
+    try:
+        book = book_paginator.page(book_page)
+    except PageNotAnInteger:
+        book = book_paginator.page(1)
+    except EmptyPage:
+        # if ?page over than number of pages, then set page = last page of pagination
+        book = book_paginator.page(book_paginator.num_pages)
+    except InvalidPage:
+        book = book_paginator.page(1)
+
     return render(request, 'book/index.html', {'book':book, 'categories': categories, 'category_id':category_id})
