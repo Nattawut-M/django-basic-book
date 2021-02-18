6a672950caf6f4a0c28d6d479913e03a9dd189c1
Nattawut Meesamsen <t.nattawut2020@hotmail.com>
2021-02-18 15:26:22 +0700

    <check premission to access 'path'> [book/urls.py] import 'login_required' from 'django.contrib.auth.decorators' then usge funcion in path() add 2 arguments 1. views.def 2.login_url//

--- book/urls.py
+++ book/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, re_path
+# check premission to access path
+from django.contrib.auth.decorators import login_required
+
+
 from  . import views
 urlpatterns = [
     path('', views.index, name="home"),
-    path('book_detail/<slug:slug>', views.book_detail, name='book-detail'),
-    re_path(r'^book_add/$', views.book_add, name='book-add'),
+    path('book_detail/<slug:slug>', login_required(views.book_detail, login_url='book:login'), name='book-detail'),
+    re_path(r'^book_add/$', login_required(views.book_add, login_url='book:login'), name='book-add'), # login before access path
     path('login', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('signup', views.signup_view, name = 'signup'),
